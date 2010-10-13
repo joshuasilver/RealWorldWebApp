4299c63e7cb4ee924934997880864b4a05a80122
Joshua Silver <joshua.silver@securehealthpay.com>
2010-10-13 20:05:01 +0000

    Added validation to phone number

--- app/models/student.php
+++ app/models/student.php
@@ -2,6 +2,13 @@
 
 class Student extends AppModel {
     var $name = 'Student';
+
+    var $validate = array(
+        'phone_number' => array(
+            'rule' => array('phone', null, 'us'),
+            'message' => "Must be a valid US phone number"
+        )
+    );
 }
 
 ?>
